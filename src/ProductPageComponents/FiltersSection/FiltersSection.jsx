1fa3709b5f010e4f03c5ecf08953b15085ef1916
fallienop <ulvi.babayev.200@gmail.com>
2023-12-23 01:47:56 +0400

    changes

--- src/ProductPageComponents/FiltersSection/FiltersSection.jsx
+++ src/ProductPageComponents/FiltersSection/FiltersSection.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getSelectedFilters,resetState } from '../../Redux/techshopSlicer';
+import { getSelectedFilters, resetState } from '../../Redux/techshopSlicer';
 import style from './FiltersSection.module.css';
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
@@ -14,29 +14,50 @@ const FiltersSection = () => {
   const [showPriceSection, setPriceSection] = useState(false);
   const [filters, setFilters] = useState([]);
   const animatedComponents = makeAnimated();
-  const priceReturner=()=> {
-    if(showPriceSection){
-    return <div className={style.priceinputBox}>
+
+  const priceReturner = () => {
+    if (showPriceSection) {
+      return <div className={style.priceinputBox}>
         <p>price</p>
-      <div className={style.priceinput}>
-        <input onInput={(e)=>inputNumberValidator(e)} type='text' placeholder='min'></input>
-        <input onInput={(e)=>inputNumberValidator(e)} type='text'  placeholder='max'></input>
+        <div className={style.priceinput}>
+          <input onInput={(e) => inputNumberValidator(e)} type='text' placeholder='min'></input>
+          <input onInput={(e) => inputNumberValidator(e)} type='text' placeholder='max'></input>
+        </div>
       </div>
-      </div>}
     }
-
-const inputNumberValidator = (e)=>{
-  const inputValue = e.target.value;
-  const regex = /^\d+(\.\d{0,4})?$/;
-
-  if (!inputValue.match(regex)) {
-    // Remove the last character if it doesn't match the regex
-    e.target.value = inputValue.slice(0, -1);
   }
-}
 
-  const Valuereturner = () => {
+  const inputNumberValidator = (e) => {
+    const inputValue = e.target.value;
+    const regex = /^\d+(\.\d{0,4})?$/;
+
+    if (!inputValue.match(regex)) {
+      // Remove the last character if it doesn't match the regex
+      e.target.value = inputValue.slice(0, -1);
+    }
+  }
+
+  const nameByIdReturner = (category, val) => {
+
+    return fetch(`https://localhost:7167/${category}names/${val}`,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+
+    })
+      .then(apiResponse => apiResponse.json())
+      .then((data) => (
+        {
+        label: data,
+        value: val
+      }));
+  }
+
+  const Valuereturner = async () => {
+
     let filtersArr = {};
+
 
     if (rawFilters[0] !== undefined) {
       if (Object.keys(rawFilters[0]).length === 1) {
@@ -44,7 +65,7 @@ const inputNumberValidator = (e)=>{
 
         const categoryKey = Object.keys(rawFilters[0])[0];
         const categoryValues = rawFilters[0][categoryKey];
-        
+
         filtersArr[categoryKey] = categoryValues.map(x => ({
           label: [reduxCategories[x]],
           value: x
@@ -52,18 +73,29 @@ const inputNumberValidator = (e)=>{
       } else {
         setPriceSection(true);
         let rawfilter = rawFilters[0];
-          
-        Object.keys(rawfilter).forEach(filterkey => {
-          if(filterkey!='categoryId'&&filterkey!='price'){
-          const filterValues = rawfilter[filterkey];
+        await Promise.all(
+          Object.keys(rawfilter).map(async (filterkey) => {
+            if (filterkey != 'categoryId' && filterkey != 'price') {
+              const filterValues = rawfilter[filterkey];
 
-          filtersArr[filterkey] = filterValues.map(x => ({
-            label: x,
-            value: x
+
+              filtersArr[filterkey] = await Promise.all(
+                filterValues.map(async (x) => {
+                  return filterkey.endsWith('Id') ? await nameByIdReturner(filterkey, x) : {
+                    label: x,
+                    value: x
+                  };
+
+
+
+                })
+                );
+
+
+            }
           }));
-        }});
-       
-       
+
+
       }
     }
 
@@ -71,7 +103,7 @@ const inputNumberValidator = (e)=>{
   };
 
 
-   
+
   const handleSelectChange = (filterKey, selecteddOptions) => {
     const values = selecteddOptions.map(obj => obj.value);
     setSelectedOptions({ [filterKey]: values });
@@ -85,11 +117,10 @@ const inputNumberValidator = (e)=>{
 
 
   useEffect(() => {
-
     if (rawFilters !== undefined) {
-      setFilters(Valuereturner())
-
-
+      Valuereturner().then((result) => {
+        setFilters(result);
+      });
     }
   }, [rawFilters])
 
@@ -98,8 +129,8 @@ const inputNumberValidator = (e)=>{
     <div className={style.main}>
       <span id='filterapplier' onClick={sendSelectedFilters} className={style.applyfilterbutton}>Apply Filters</span>
 
-      {Object.keys(filters).map((filterKey) => (
-        <div className={style.filtes}>
+      {Object.keys(filters).map((filterKey, ind) => (
+        <div key={ind} className={style.filtes}>
           <p className={style.filtername}>{filterKey}</p>
           <Select
             key={filterKey}
@@ -111,16 +142,16 @@ const inputNumberValidator = (e)=>{
               handleSelectChange(filterKey, selectedOptions)
             }
           />
-          
-         
+
+
         </div>
       ))}
 
-  {priceReturner()}
+      {priceReturner()}
 
-  <span className={style.reseter} onClick={()=>{ window.location.reload()}}>Reset</span>
-</div>
-  
+      <span className={style.reseter} onClick={() => { window.location.reload() }}>Reset</span>
+    </div>
+
   );
 };
 
